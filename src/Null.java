bf46a906e458af7620b74966da65049e48e129ca
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2018-11-24 11:41:51 +0900

    report4

--- src/Null.java
+++ src/Null.java
@@ -4,7 +4,16 @@ public class Null {
 
     public static void main(String[] args){
 
-        System.out.println(str.length());
+        try {
+
+            System.out.println(str.length());
+
+        }catch (Exception e){
+
+            System.out.println(e);
+            System.out.println(e.getMessage());
+        }
+
 
     }
 }
