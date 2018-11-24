4c72188ae99e27f7bb34e291e9a3b922718de087
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2018-11-24 11:50:15 +0900

    report4

--- src/Null.java
+++ src/Null.java
@@ -4,16 +4,7 @@ public class Null {
 
     public static void main(String[] args){
 
-        try {
-
-            System.out.println(str.length());
-
-        }catch (Exception e){
-
-            System.out.println(e);
-            System.out.println(e.getMessage());
-        }
-
-
+        str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
